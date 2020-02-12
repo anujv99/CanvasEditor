a2d4bec1449f78eb0d4adec5066fa99bf7d2d1e9
preversewharf45 <preverse45you@gmail.com>
2020-02-12 18:46:20 +0530

    gg

--- app/src/core/timer.cpp
+++ app/src/core/timer.cpp
@@ -4,8 +4,12 @@
 
 namespace app { namespace core {
 
+#ifdef __WINDOWS__
 	static std::chrono::time_point<std::chrono::steady_clock> START_TIME = std::chrono::high_resolution_clock::now();
-
+#elif __UNIX__
+	static std::chrono::time_point<std::chrono::_V2::system_clock> START_TIME = std::chrono::high_resolution_clock::now();
+#endif
+	
 	TimePoint Timer::s_PrevTime			= 0.0f;
 	TimePoint Timer::s_CurrentTime		= 0.0f;
 	TimePoint Timer::s_DeltaTime		= 0.0f;
